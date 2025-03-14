0f0d39767a0dab712fee79934e29d4b5439eed2f
calvingolas <calvingolas@gmail.com>
2025-03-14 01:22:52 -0700

    Finished csv writing

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,16 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
   public static void main(String[] args) {
-    String instructions = """
-        Essentially, this is the idea.
-        Different dates stay in separate rows (column 1)
-        Different Marshes stay in separate rows (column 2)
-        Different Traps (within marshes and dates) stay in separate rows (column 3
-        Different Species stay in separate rows (column 4)
-        
-        Essentially, all instances of a Spp (species) that are in the same trap, same marsh, and same date should be put in the same row. For all of the rows that get condensed into one row, a new column should be made to the right with the total number of rows that got condensed. For example, if 5 rows all have the same trap#, spp, and date, a new column in the same row should have 5.
-        
-        You should keep the columns Date, Marsh, Trap#, and Spp. The rest of the rows can be disregarded.
-        """;
+    // This hashmap will contain the trap entries keyed off of composite with number in each
+    Map<String, Integer> trapMap = new HashMap<>();
 
     BufferedReader reader;
     try {
@@ -28,15 +22,36 @@ public class Main {
       while (reader.ready()) {
         String entry = reader.readLine();
         String[] splitEntry = entry.split(",");
-        TrapEntry trapEntry = new TrapEntry(splitEntry[0], splitEntry[1], splitEntry[2], splitEntry[3]);
-        System.out.println(splitEntry[1]);
+        for (int i = 0; i < splitEntry.length; i++) {
+          splitEntry[i] = splitEntry[i].trim();
+        }
+        TrapEntry trapEntry = new TrapEntry(splitEntry[1], splitEntry[2], splitEntry[3], splitEntry[4]);
+        String compositeKey = trapEntry.getCompositeKy();
+        trapMap.put(compositeKey, trapMap.getOrDefault(compositeKey, 0) + 1);
       }
-
       reader.close();
     } catch (IOException e) {
       System.out.println("Failed to read input.csv file in repo");
       throw new RuntimeException(e);
     }
 
+    // now output in new CSV
+    try {
+      BufferedWriter writer = Files.newBufferedWriter(Paths.get("output.csv"));
+      // Start by writing the column names
+      writer.write("Date,Marsh,Trap#,Species,Count");
+      writer.newLine();
+
+      for (String compositeKey : trapMap.keySet()) {
+        TrapEntry trapEntry = TrapEntry.splitCompositeKey(compositeKey);
+        int trappedCount = trapMap.get(compositeKey);
+        writer.write(trapEntry.date() + "," + trapEntry.marsh() + "," + trapEntry.trapName() + "," + trapEntry.species() + "," + trappedCount);
+        writer.newLine();
+      }
+      writer.close();
+    } catch (IOException e) {
+      System.out.println("Failed to write to output.csv in repo");
+      throw new RuntimeException(e);
+    }
   }
 }
--- src/TrapEntry.java
+++ src/TrapEntry.java
@@ -1,5 +1,11 @@
 public record TrapEntry(String date, String marsh, String trapName, String species) {
+  static String COMPOSITE_KEY_FORMATTED_STRING = "%s,%s,%s,%s";
+
   public String getCompositeKy() {
-    return date + marsh + trapName + species;
+    return String.format(COMPOSITE_KEY_FORMATTED_STRING, date, marsh, trapName, species);
+  }
+  public static TrapEntry splitCompositeKey(String compositeKey) {
+    String[] splitKey = compositeKey.split(",");
+    return new TrapEntry(splitKey[0], splitKey[1], splitKey[2], splitKey[3]);
   }
 }
